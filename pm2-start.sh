8c4a559a3acbe7ae3af139dda45849ffacdc693b
manh-vv <manh.vv.htth@gmail.com>
2019-10-25 16:45:31 +0700

    sync-code with upstream

--- pm2-start.sh
+++ pm2-start.sh
@@ -3,12 +3,12 @@
 alias pm2='authbind --deep pm2'
 
 echo "cleaning up"
-pm2 del all
+pm2 del api
 rm -f ~/.pm2/dump*.pm2
-rm -f ~/.pm2/logs/*.log
+rm -f ~/.pm2/logs/api-*.log
 pm2 update
 
 echo "cleaning up done"
 
 echo "start app"
-pm2 start --watch dist ./config/ecosystem.config.js
+pm2 start ./config/ecosystem.config.js
